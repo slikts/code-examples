abd37e5eff177ad2a714ff575e94ad81dccd4754
Reinis Ivanovs <rivanovs@scoutrfp.com>
2019-01-30 15:24:00 +0200

    add issue

---
+++ src/bar.js
@@ -0,0 +1,5 @@
+export const bar = (baz) => {
+  return baz ? 1 : (baz ? 3 : 4);
+
+  console.log(100) // unreachable
+}
--- src/foo.js
+++ src/foo.js
@@ -2,6 +2,7 @@ while (true) {
   alert(101);
 }
 
+// unreachable
 if (window.bla) {
   console.log(window.bla ? 123 : 456);
 }
